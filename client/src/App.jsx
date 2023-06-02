418d3e74be49a13367d69d9b7c12beb4f5b60e9c
Anna Lefour <anka.piekarnik@gmail.com>
2023-06-02 11:23:45 +0200

    Push with hot reload problem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Je change le texte !</p>
+        <p>Je change encore !</p>
       </header>
     </div>
   );
